27d6734b6fbd80e7a0b4b003c565e418d1a9ce76
showrab09 <showrabbinhabib@gmail.com>
2021-09-02 06:13:58 +0600

    Full Project Complete

--- js/app.js
+++ js/app.js
@@ -1,33 +1,53 @@
 const searchInput = document.getElementById('searchData');
 const displayBox = document.getElementById('displayBox');
-
+const resultBox = document.getElementById('result-box');
+//loading the api data
 const searchButton = () => {
-    const url = `http://openlibrary.org/search.json?q=${searchInput.value}`
+    const url = `https://openlibrary.org/search.json?q=${searchInput.value}`
     fetch(url)
         .then(response => response.json())
-        .then(data => displayData(data.docs))
-}
+        .then(data => displayData(data))
+};
+
 const displayData = (books) => {
-    books.forEach(book => {
-        console.log(book);
-        const div = document.createElement("div");
-        div.classList.add("book");
-        div.innerHTML = `  
-         <div class="row border mb-3">
-                <div class="col-md-4">
-                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded-start"
-                        alt="...">
+    //removing the previous data
+    searchInput.value = '';
+    displayBox.innerHTML = '';
+    resultBox.innerHTML = '';
+    //counting the number of books
+    let count = 0;
+    books.docs.forEach(book => {
+        //checking if the exact named book is present or not
+        if (book.has_fulltext === true) {
+            count++;
+            //Sizing the loaded data
+            const author = book.author_name.splice(0, 1);
+            const title = book.title.slice(0, 20);
+            const firstPublish = book.first_publish_year;
+            const publisher = book.publisher.splice(0, 1);
+            const div = document.createElement("div");
+            div.classList.add("book");
+            div.innerHTML = `
+         <div class="row  my-2 mx-1  rounded-3">
+                <div class="col-md-4 my-auto">
+                    <img  width="120px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class=" rounded-start  " alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">${book.title}</h5>
-                        <p class="card-text">Author: ${book.author_name[0]}</p>
-                        <p class="card-text">${book.first_publish_year}</p>
+                        <h4 class="card-title">${title}</h4>
+                        <p class="card-text"><b>Author:</b> ${author}</p>
+                        <p class="card-text"><b>First publish:</b> ${firstPublish}</p>
+                        <p class="card-text"><b>Publisher:</b> ${publisher}</p>
                     </div>
                 </div>
             </div>
         `;
-        displayBox.appendChild(div);
-
+            displayBox.appendChild(div);
+        }
     })
+    // finding the total result.. here I used numFound value and Count value.
+    // here if my input field is empty then it will show the total result 0 and if there is no search result then it will also show the total result as 0. that's why I didn't use any other error handling. 
+    const div2 = document.createElement("div");
+    div2.innerHTML = ` <h4 class="text-success">${books.numFound} result found, ${count}  displaying</h4>`;
+    resultBox.appendChild(div2);
 }
