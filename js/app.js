4a0a3133d747e9793e31dcb1298db9e2a3d1a3a0
showrab09 <showrabbinhabib@gmail.com>
2021-09-02 21:22:20 +0600

    Bug fixed

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ const searchButton = () => {
             .then(data => displayData(data))
     }
 };
-
 const displayData = (books) => {
     //removing the previous data
     searchInput.value = '';
@@ -23,18 +22,21 @@ const displayData = (books) => {
     //counting the number of books
     let count = 0;
     books.docs.forEach(book => {
-
         //checking if the exact named book is present or not
         if (book.has_fulltext === true) {
-            count++;
-            //Sizing the loaded data
-            const author = book.author_name.splice(0, 1);
-            const title = book.title.slice(0, 20);
-            const firstPublish = book.first_publish_year;
-            const publisher = book.publisher.splice(0, 1);
-            const div = document.createElement("div");
-            div.classList.add("book");
-            div.innerHTML = `
+            //skipping the undefined data 
+            if (typeof (book.publisher) === 'undefined' || typeof (book.author_name) === 'undefined') {
+                console.log('no publisher');
+            } else {
+                count++;
+                //Sizing the loaded data
+                const author = book.author_name.splice(0, 1);
+                const title = book.title.slice(0, 20);
+                const firstPublish = book.first_publish_year;
+                const publisher = book.publisher.splice(0, 1);
+                const div = document.createElement("div");
+                div.classList.add("book");
+                div.innerHTML = `
          <div class="row  my-2 mx-1  rounded-3">
                 <div class="col-md-4 my-auto">
                     <img  width="120px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class=" rounded-start  " alt="...">
@@ -49,7 +51,8 @@ const displayData = (books) => {
                 </div>
             </div>
         `;
-            displayBox.appendChild(div);
+                displayBox.appendChild(div);
+            }
         }
     })
     // finding the total result.. here I used numFound value and Count value. 
